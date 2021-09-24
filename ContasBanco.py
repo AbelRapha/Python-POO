ab878efb5b4eaf474c1445fc2681075f4e399377
Abel <abelrapha@gmail.com>
2021-09-24 00:37:34 -0300

    arquivo main.py

--- ContasBanco.py
+++ ContasBanco.py
@@ -87,12 +87,3 @@ class CartaoCredito:
         conta_corrente.cartoes.append(self)
 
 
-#programa
-conta_abel = ContaCorrente('Abel', '000.111.222-33', '0001', '0001009-2')
-
-cartao_abel = CartaoCredito("Abel",conta_abel)
-
-print(cartao_abel.conta_corrente._conta)
-print(cartao_abel.numero)
-print(cartao_abel.Validade)
-print(cartao_abel.cvc)
